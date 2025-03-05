cf99a128a79c44a2398d7ea7642fe109e84c7a51
jamshidbazarbaev5 <your-github-email@example.com>
2025-03-06 00:56:47 +0500

    added /ru,en/,kk,/uz endpoints

---
+++ src/app/[lang]/file/page.tsx
@@ -0,0 +1,5 @@
+import NormativeDocuments from "../../pages/Files";
+import './main.css'
+export default function File() {
+    return <NormativeDocuments/>
+}
--- src/app/file/page.tsx
+++
@@ -1,5 +0,0 @@
-import NormativeDocuments from "../pages/Files";
-import '../file/main.css'
-export default function File() {
-    return <NormativeDocuments/>
-}
