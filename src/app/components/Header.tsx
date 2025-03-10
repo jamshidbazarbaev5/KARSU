72deb00c4193e5bb1c24e8d0af175958714cae1f
jamshidbazarbaev5 <your-github-email@example.com>
2025-03-10 18:42:21 +0500

    added error modal

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -312,6 +312,18 @@ const Header = () => {
                     </div>
                     <div className='header-main-left'>
                         <div className='header-main-left-lang'>
+                            <svg className="top-links__svg" 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                width="16px" 
+                                height="16px" 
+                                viewBox="0 0 24 24">
+                                <path 
+                                    fill="none" 
+                                    stroke="#ffffff" 
+                                    strokeWidth="2" 
+                                    d="M12,23 C18.0751322,23 23,18.0751322 23,12 C23,5.92486775 18.0751322,1 12,1 C5.92486775,1 1,5.92486775 1,12 C1,18.0751322 5.92486775,23 12,23 Z M12,23 C15,23 16,18 16,12 C16,6 15,1 12,1 C9,1 8,6 8,12 C8,18 9,23 12,23 Z M2,16 L22,16 M2,8 L22,8"
+                                />
+                            </svg>
                             <select 
                                 name="language" 
                                 id="language"
@@ -324,7 +336,6 @@ const Header = () => {
                                 <option value="RU">{t('header.languages.ru')}</option>
                                 <option value="EN">{t('header.languages.en')}</option>
                             </select>
-                           
                         </div>
                         <form onSubmit={handleSearch} className='header-main-left-search'>
                             <input type="checkbox" id="search_call" />
