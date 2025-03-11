e6dbc265d570b928d7e320a5b5f88651bbe544b4
jamshidbazarbaev5 <your-github-email@example.com>
2025-03-11 07:36:57 +0500

    added date

--- src/app/[lang]/menus/main/[slug]/page.tsx
+++ src/app/[lang]/menus/main/[slug]/page.tsx
@@ -218,8 +218,7 @@ export default function MenuPage() {
             <Link href={`/${i18n.language}`}>{t("common.home")}</Link> •
             <Link href={`/${i18n.language}/menus`}>{t("common.menus")}</Link> •
             <span>
-              {menuItem?.translations[i18n.language as SupportedLanguages]
-                ?.title || menuItem?.translations[i18n.language as SupportedLanguages].slug}
+              { menuItem?.translations[i18n.language as SupportedLanguages].slug}
             </span>
           </nav>
           
